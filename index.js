c45ad42f285a5407ac8b485a3954f34d020c9bc1
Timothy Losper <tim_losper@yahoo.com>
2022-08-15 12:09:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -131,30 +131,39 @@ app.get(
 }*/
 app.post(
   "/users",
+  // Validation logic here for request
+  //you can either use a chain of methods like .not().isEmpty()
+  //which means "opposite of isEmpty" in plain english "is not empty"
+  //or use .isLength({min: 5}) which means
+  //minimum value of 5 characters are only allowed
   [
-    check("Username", "Username is required").isLength({ min: 5 }),
-    Check(
+    check("Name", "Username is required").isLength({ min: 5 }),
+    check(
       "Name",
       "Username contains non alphanumeric characters - not allowed."
     ).isAlphanumeric(),
-    check("Password", "Password is required.")
+    check("Password", "Password is required")
       .not()
       .isEmpty(),
-    check("Email", "Email does not appear to be valid.").isEmail()
+    check("Email", "Email does not appear to be valid").isEmail()
   ],
   (req, res) => {
+    // check the validation object for errors
     let errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(422).json({ errors: errors.array() });
     }
+
     let hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Name: req.body.Name })
+    Users.findOne({ Name: req.body.Name }) // Search to see if a user with the requested username already exists
       .then(user => {
         if (user) {
-          return res.status(400).send(req.body.Name + " already exists");
+          //If the user is found, send a response that it already exists
+          return res.status(400).send(req.body.Username + " already exists");
         } else {
           Users.create({
-            Name: req.body.Name,
+            Username: req.body.Name,
             Password: hashedPassword,
             Email: req.body.Email,
             Birthday: req.body.Birthday
@@ -174,6 +183,51 @@ app.post(
       });
   }
 );
+// app.post(
+//   "/users",
+//   [
+//     check("Name", "Username is required").isLength({ min: 5 }),
+//     Check(
+//       "Name",
+//       "Username contains non alphanumeric characters - not allowed."
+//     ).isAlphanumeric(),
+//     check("Password", "Password is required.")
+//       .not()
+//       .isEmpty(),
+//     check("Email", "Email does not appear to be valid.").isEmail()
+//   ],
+//   (req, res) => {
+//     let errors = validationResult(req);
+//     if (!errors.isEmpty()) {
+//       return res.status(422).json({ errors: errors.array() });
+//     }
+//     let hashedPassword = Users.hashPassword(req.body.Password);
+//     Users.findOne({ Name: req.body.Name })
+//       .then(user => {
+//         if (user) {
+//           return res.status(400).send(req.body.Name + " already exists");
+//         } else {
+//           Users.create({
+//             Name: req.body.Name,
+//             Password: hashedPassword,
+//             Email: req.body.Email,
+//             Birthday: req.body.Birthday
+//           })
+//             .then(user => {
+//               res.status(201).json(user);
+//             })
+//             .catch(error => {
+//               console.error(error);
+//               res.status(500).send("Error: " + error);
+//             });
+//         }
+//       })
+//       .catch(error => {
+//         console.error(error);
+//         res.status(500).send("Error: " + error);
+//       });
+//   }
+// );
 
 //Update user information
 app.put(
