a1315e22f63c8140a12255fa3b21351895e130f9
Timothy Losper <tim_losper@yahoo.com>
2022-08-15 12:38:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ app.post(
   //or use .isLength({min: 5}) which means
   //minimum value of 5 characters are only allowed
   [
-    check("Username", "Username is required").isLength({ min: 5 }),
+    check("Name", "Username is required").isLength({ min: 5 }),
     check(
       "Name",
       "Username contains non alphanumeric characters - not allowed."
@@ -233,7 +233,7 @@ app.post(
 app.put(
   "/users/:Name",
   [
-    check("Username", "Username is required").isLength({ min: 5 }),
+    check("Name", "Username is required").isLength({ min: 5 }),
     Check(
       "Name",
       "Username contains non alphanumeric characters - not allowed."
