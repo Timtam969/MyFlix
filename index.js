5ff8a2a54a7e6ca633c56da14b6380f0715f40bd
Timothy Losper <tim_losper@yahoo.com>
2022-08-16 15:09:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -74,20 +74,16 @@ app.get("/documentation", (req, res) => {
 });
 
 // Read endpoint: Returns all movies to the user.
-app.get(
-  "/movies",
-  passport.authenticate("jwt", { session: false }),
-  (req, res) => {
-    Movies.find()
-      .then(movies => {
-        res.status(200).json(movies);
-      })
-      .catch(err => {
-        console.error(err);
-        res.status(500).send("Error: " + err);
-      });
-  }
-);
+app.get("/movies", (req, res) => {
+  Movies.find()
+    .then(movies => {
+      res.status(200).json(movies);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).send("Error: " + err);
+    });
+});
 
 // Read endpoint: Returns all movie to the user by title search.
 app.get(
