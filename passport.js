48f3669d181749473a76d40ea0d407b471754cae
Timothy Losper <tim_losper@yahoo.com>
2022-08-17 07:40:17 +0200

    Update passport.js

--- passport.js
+++ passport.js
@@ -1,75 +1,12 @@
-// const passport = require("passport"),
-//   LocalStrategy = require("passport-local").Strategy,
-//   Models = require("./models.js"),
-//   passportJWT = require("passport-jwt");
-//
-// let Users = Models.User,
-//   JWTStrategy = passportJWT.Strategy,
-//   ExtractJWT = passportJWT.ExtractJwt;
-//
-// passport.use(
-//   new LocalStrategy(
-//     {
-//       usernameField: "Username",
-//       passwordField: "Password"
-//     },
-//     (username, password, callback) => {
-//       console.log(username + "  " + password);
-//       Users.findOne({ Username: username }, (error, user) => {
-//         if (error) {
-//           console.log(error);
-//           return callback(error);
-//         }
-//
-//         if (!user) {
-//           console.log("incorrect username");
-//           return callback(null, false, {
-//             message: "Incorrect username or password."
-//           });
-//         }
-//
-//         if (!user.validatePassword(password)) {
-//           console.log("Incorrect password");
-//           return callback(null, false, { message: "Incorrect Password." });
-//         }
-//
-//         console.log("finished");
-//         return callback(null, user);
-//       });
-//     }
-//   )
-// );
-//
-// passport.use(
-//   new JWTStrategy(
-//     {
-//       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-//       secretOrKey: "your_jwt_secret"
-//     },
-//     (jwtPayload, callback) => {
-//       return Users.findById(jwtPayload._id)
-//         .then(user => {
-//           return callback(null, user);
-//         })
-//         .catch(error => {
-//           return callback(error);
-//         });
-//     }
-//   )
-// );
-
 const passport = require("passport"),
-  //defines basic HTTP authentication for login requests.
   LocalStrategy = require("passport-local").Strategy,
   Models = require("./models.js"),
   passportJWT = require("passport-jwt");
 
 let Users = Models.User,
-  //set up JWT authentication code, it allows you to authenticate uers based on the JWT submitted alongside their request.
   JWTStrategy = passportJWT.Strategy,
   ExtractJWT = passportJWT.ExtractJwt;
 
-//only check for username here, no password check yet
 passport.use(
   new LocalStrategy(
     {
@@ -77,7 +14,7 @@ passport.use(
       passwordField: "Password"
     },
     (username, password, callback) => {
-      console.log(username + " " + password);
+      console.log(username + "  " + password);
       Users.findOne({ Username: username }, (error, user) => {
         if (error) {
           console.log(error);
@@ -86,15 +23,14 @@ passport.use(
 
         if (!user) {
           console.log("incorrect username");
-          //if an error occurs, or if the username can't be found within the database, an error message is passed to the callback
           return callback(null, false, {
-            message: "Incorrect username or password"
+            message: "Incorrect username or password."
           });
         }
 
         if (!user.validatePassword(password)) {
-          console.log("incorrect password");
-          return callback(null, false, { message: "Incorrect password." });
+          console.log("Incorrect password");
+          return callback(null, false, { message: "Incorrect Password." });
         }
 
         console.log("finished");
@@ -107,7 +43,6 @@ passport.use(
 passport.use(
   new JWTStrategy(
     {
-      //In the code below, the JWT is extracted from the header of the HTTP request
       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
       secretOrKey: "your_jwt_secret"
     },
