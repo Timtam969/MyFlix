6be01370171d98a8cb20b2e537de3487ed91708f
Timothy Losper <tim_losper@yahoo.com>
2023-01-02 18:30:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,30 @@ const bodyParser = require("body-parser");
 const uuid = require("uuid");
 
 const morgan = require("morgan");
+const cors = require("cors");
+let allowedOrigins = [
+  "http://localhost:8080",
+  "http://testsite.com",
+  "http://localhost:1234",
+  "http://localhost:4200"
+];
+const { check, validationResult } = require("express-validator");
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        // If a specific origin isn’t found on the list of allowed origins
+        let message =
+          "The CORS policy for this application doesn’t allow access from origin " +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    }
+  })
+);
 const app = express();
 const mongoose = require("mongoose");
 const Models = require("./models.js");
@@ -30,31 +54,6 @@ app.use(morgan("combined", { stream: accessLogStream }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
-
-const cors = require("cors");
-let allowedOrigins = [
-  "http://localhost:8080",
-  "http://testsite.com",
-  "http://localhost:1234",
-  "http://localhost:4200"
-];
-const { check, validationResult } = require("express-validator");
-
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        // If a specific origin isn’t found on the list of allowed origins
-        let message =
-          "The CORS policy for this application doesn’t allow access from origin " +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    }
-  })
-);
 
 let auth = require("./auth")(app);
 
